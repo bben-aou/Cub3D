f51b16c402706b01edaa21adbf67a50534f3e9ec
omayrib <iomayr@student.1337.ma>
2022-11-13 16:16:08 +0100

    protect the conversions from xpm to image

--- raycasting/rendering3D.c
+++ raycasting/rendering3D.c
@@ -6,7 +6,7 @@
 /*   By: iomayr <iomayr@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:28:45 by bben-aou          #+#    #+#             */
-/*   Updated: 2022/11/13 14:19:16 by iomayr           ###   ########.fr       */
+/*   Updated: 2022/11/13 16:10:27 by iomayr           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,31 +57,45 @@ void get_intersection(t_var *var)
     get_intersection_vert(var);
 }
 
+void init_rest_of_textures(t_var *var)
+{
+
+    var->e_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->east_texture->path,
+		&var->texture->east_texture->width_txt, &var->texture->east_texture->height_txt);
+	if (!var->e_texture)
+		error_map("Occured during convert Third texture to img", var);
+	
+	var->texture->east_texture->img->addr = (int *)mlx_get_data_addr(var->e_texture,
+        &var->texture->east_texture->img->bpp, &var->texture->east_texture->img->line_len,
+        &var->texture->east_texture->img->endian);
+
+    var->w_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->west_texture->path,
+        &var->texture->west_texture->width_txt, &var->texture->west_texture->height_txt);
+    if (!var->w_texture)
+		error_map("Occured during convert Fourth texture to img", var);
+	var->texture->west_texture->img->addr = (int *)mlx_get_data_addr(var->w_texture,
+        &var->texture->west_texture->img->bpp, &var->texture->west_texture->img->line_len,
+    	&var->texture->west_texture->img->endian);
+}
+
 void init_texture_img(t_var *var)
 {
     var->n_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->north_texture->path,
-                                           &var->texture->north_texture->width_txt, &var->texture->north_texture->height_txt);
-    var->texture->north_texture->img->addr = (int *)mlx_get_data_addr(var->n_texture,
-                                                                      &var->texture->north_texture->img->bpp, &var->texture->north_texture->img->line_len,
-                                                                      &var->texture->north_texture->img->endian);
+		&var->texture->north_texture->width_txt, &var->texture->north_texture->height_txt);
+    
+	var->texture->north_texture->img->addr = (int *)mlx_get_data_addr(var->n_texture,
+		&var->texture->north_texture->img->bpp, &var->texture->north_texture->img->line_len,
+		&var->texture->north_texture->img->endian);
+    
+	var->s_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->south_texture->path,
+        &var->texture->south_texture->width_txt, &var->texture->south_texture->height_txt);
+	if (!var->s_texture)
+		error_map("Occured during convert second texture to img", var);
 
-    var->s_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->south_texture->path,
-                                           &var->texture->south_texture->width_txt, &var->texture->south_texture->height_txt);
-    var->texture->south_texture->img->addr = (int *)mlx_get_data_addr(var->s_texture,
-                                                                      &var->texture->south_texture->img->bpp, &var->texture->south_texture->img->line_len,
-                                                                      &var->texture->south_texture->img->endian);
-
-    var->e_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->east_texture->path,
-                                           &var->texture->east_texture->width_txt, &var->texture->east_texture->height_txt);
-    var->texture->east_texture->img->addr = (int *)mlx_get_data_addr(var->e_texture,
-                                                                     &var->texture->east_texture->img->bpp, &var->texture->east_texture->img->line_len,
-                                                                     &var->texture->east_texture->img->endian);
-
-    var->w_texture = mlx_xpm_file_to_image(var->mlx->init, var->texture->west_texture->path,
-                                           &var->texture->west_texture->width_txt, &var->texture->west_texture->height_txt);
-    var->texture->west_texture->img->addr = (int *)mlx_get_data_addr(var->w_texture,
-                                                                     &var->texture->west_texture->img->bpp, &var->texture->west_texture->img->line_len,
-                                                                     &var->texture->west_texture->img->endian);
+	var->texture->south_texture->img->addr = (int *)mlx_get_data_addr(var->s_texture,
+		&var->texture->south_texture->img->bpp, &var->texture->south_texture->img->line_len,
+		&var->texture->south_texture->img->endian);
+	init_rest_of_textures(var);
 }
 
 void calcul_for_3d(t_var *var)
