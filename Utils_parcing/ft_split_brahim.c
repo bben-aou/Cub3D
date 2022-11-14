13964445fd6e1da75c76f9ea4a3c727530aaf74d
omayrib <iomayr@student.1337.ma>
2022-11-14 15:41:50 +0100

    another part in norm before applying leaks

--- Utils_parcing/ft_split_brahim.c
+++ Utils_parcing/ft_split_brahim.c
@@ -6,7 +6,7 @@
 /*   By: iomayr <iomayr@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 11:10:59 by iomayr            #+#    #+#             */
-/*   Updated: 2022/11/14 15:40:31 by iomayr           ###   ########.fr       */
+/*   Updated: 2022/11/14 15:41:43 by iomayr           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ char	**ft_split_b(char *s, char c)
 		else if ((s[p] == c || p == ft_strlen1(s)) && i >= 0)
 		{
 			ptr[j++] = ft_print(s, i, p);
-			i = -1;
+			i = -1; 
 		}
 	}
 	ptr[j] = 0;
